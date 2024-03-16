791a889a91f1f99bf5c44a49c35f73027a81bf05
Kevin Pierce <kpierce236@gmail.com>
2024-03-16 17:33:24 -0400

    fixed user models and views

--- models/User.js
+++ models/User.js
@@ -2,12 +2,6 @@ const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
-class User extends Model {
-  checkPassword(loginPw) {
-    return bcrypt.compareSync(loginPw, this.password);
-  }
-}
-
 User.init(
   {
     id: {
@@ -37,12 +31,7 @@ User.init(
     },
   },
   {
-    hooks: {
-      beforeCreate: async (newUserData) => {
-        newUserData.password = await bcrypt.hash(newUserData.password, 10);
-        return newUserData;
-      },
-    },
+   
     sequelize,
     timestamps: false,
     freezeTableName: true,
